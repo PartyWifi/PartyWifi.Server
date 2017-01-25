c3f92609f8d6350f8af4f83050e223c200044f0f
dbeuchler <dennis@beuchler.net>
2017-01-25 20:11:09 +0100

    initial asp.net core check in

---
+++ src/WeFiBox.Web/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeFiBox.Web.Controllers
+{
+    public class FileListEntry
+    {
+        public string Name { get; set; }
+
+        public long Size { get; set; }
+
+        public DateTime UploadDate { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FileList()
+        {
+            var uploadsDir = Path.Combine(_environment.WebRootPath, "uploads");
+            var images = Directory.GetFiles(uploadsDir, "*.jpg");
+
+            var fileListEntries = images.Select(image => new FileInfo(image)).Select(fileInfo => new FileListEntry
+            {
+                Name = fileInfo.Name,
+                Size = fileInfo.Length,
+                UploadDate = fileInfo.LastWriteTime //??
+            }).ToList();
+
+            return View(fileListEntries);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/WeFiBox.Web/Controllers/UploadController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeFiBox.Web.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public UploadController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ICollection<IFormFile> files)
+        {
+            var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+            foreach (var file in files)
+            {
+                if (file.Length <= 0)
+                    continue;
+
+                var fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+                var filePath = Path.Combine(uploads, fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+
+            return View();
+        }
+    }
+}
