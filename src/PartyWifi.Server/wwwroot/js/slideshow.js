04b7cdffcbabd705b6cd951d90f76950b15d6e96
dbeuchler <dennis@beuchler.net>
2017-02-27 23:03:51 +0100

    added slideshow css and js

---
+++ src/PartyWifi.Server/wwwroot/js/slideshow.js
@@ -0,0 +1,17 @@
+﻿var latest = ''; // Latest picture known to client
+
+// Function to update image
+function update() {
+    $.get('slideshow/next/' + latest, function (data) {
+        // Show new image
+        $('#image').attr('src', 'slideshow/image/' + data.file);
+        if (data.file > latest)
+            latest = data.file; // Update latest if response is newer 
+    });
+}
+
+// Initialize client
+$.get('slideshow/init', function (data) {
+    latest = data.file;
+    setInterval(update, data.rotationMs);
+});
