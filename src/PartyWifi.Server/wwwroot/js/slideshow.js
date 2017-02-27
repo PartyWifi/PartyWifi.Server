1d490b7533809b257ba3124acd35427d64613d3d
dbeuchler <dennis@beuchler.net>
2017-02-27 23:18:18 +0100

    directly show first image

--- src/PartyWifi.Server/wwwroot/js/slideshow.js
+++ src/PartyWifi.Server/wwwroot/js/slideshow.js
@@ -1,17 +1,28 @@
 ﻿var latest = ''; // Latest picture known to client
+var rotationMs = 0;
 
 // Function to update image
 function update() {
     $.get('slideshow/next/' + latest, function (data) {
         // Show new image
-        $('#image').attr('src', 'slideshow/image/' + data.file);
+        showImage(data.file);
+
         if (data.file > latest)
             latest = data.file; // Update latest if response is newer 
+
+        setTimeout(update, rotationMs);
     });
+}
+
+function showImage(file) {
+    $('#image').attr('src', 'slideshow/image/' + file);
 }
 
 // Initialize client
 $.get('slideshow/init', function (data) {
     latest = data.file;
-    setInterval(update, data.rotationMs);
+    rotationMs = data.rotationMs;
+
+    showImage(data.file);
+    setTimeout(update, rotationMs);
 });
