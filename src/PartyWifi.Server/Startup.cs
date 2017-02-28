5d4abac20bc3fb4deac5d92861887820933d2a0e
dbeuchler <dennis@beuchler.net>
2017-02-28 18:29:36 +0100

    added swagger api documentation

--- src/PartyWifi.Server/Startup.cs
+++ src/PartyWifi.Server/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using PartyWifi.Server.Components;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace PartyWifi.Server
 {
@@ -38,6 +39,11 @@ namespace PartyWifi.Server
 
             services.AddMvc();
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "PartyWifi API v1", Version = "v1" });
+            });
+
             // Add image manager
             services.AddSingleton<IImageManager, ImageManager>();
             services.AddSingleton<ISlideshowHandler, SlideshowHandler>();
@@ -70,6 +76,12 @@ namespace PartyWifi.Server
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PartyWifi API v1");
             });
 
             imageManager.Initialize();
