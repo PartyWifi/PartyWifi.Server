e949f19ba3fb8c6e0522be4267040d0b4896521a
dbeuchler <dennis@beuchler.net>
2017-05-01 21:28:52 +0200

    slideshow image fit screen so no scroll bars appear

--- src/PartyWifi.Server/wwwroot/js/slideshow.js
+++ src/PartyWifi.Server/wwwroot/js/slideshow.js
@@ -7,11 +7,11 @@ function update() {
         // Only using fading if activated
         if (location.search.includes('fading')) {
             image.fadeOut(1000, function() {
-                image.attr('src', 'image/resized/' + slideshowImage.imageId);
+                image.css('background-image', "url(image/resized/" + slideshowImage.imageId + ")");
                 image.fadeIn(1000);
             });
         } else {
-            image.attr('src', 'image/resized/' + slideshowImage.imageId);
+            image.css('background-image', "url(image/resized/" + slideshowImage.imageId + ")");
         }       
         
         setTimeout(update, slideshowImage.rotationMs);
