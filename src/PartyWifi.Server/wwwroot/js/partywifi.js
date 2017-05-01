48200fc7a07749ac7cf8ce08c2bb0aad00bd815a
dbeuchler <dennis@beuchler.net>
2017-05-01 22:59:02 +0200

    use back button to close magnific popup

--- src/PartyWifi.Server/wwwroot/js/partywifi.js
+++ src/PartyWifi.Server/wwwroot/js/partywifi.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-  $('.image-link').magnificPopup({type:'image'});
+    $('.image-link').magnificPopup({
+        type:'image',
+        closeOnContentClick: true,
+        enableEscapeKey: true,
+        callbacks: {   
+          open: function() {
+            location.href = location.href.split('#')[0] + "#popup";
+          },
+          close: function() {
+            if (location.hash) 
+              history.go(-1);
+          }
+        }
+    });
+});
+
+$(window).on('hashchange',function() {
+    if (location.href.indexOf("#popup") < 0) {
+        $.magnificPopup.close(); 
+    }
 });
