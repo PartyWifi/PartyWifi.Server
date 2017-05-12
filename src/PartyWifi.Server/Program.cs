8fa6fb32cc033d6b47644e74d97eff49237ab445
Toxantron <thomas.fuchs91@gmail.com>
2017-05-12 13:37:13 +0200

    Use Port 80

--- src/PartyWifi.Server/Program.cs
+++ src/PartyWifi.Server/Program.cs
@@ -11,7 +11,7 @@ namespace PartyWifi.Server
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
-                .UseUrls("http://*:5000")
+                .UseUrls("http://*:80")
                 .UseStartup<Startup>()
                 .Build();
 
